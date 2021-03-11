fad96110eab9cacadc614fcebb305dbc140fcba0
krizej <60076189+krizej@users.noreply.github.com>
2021-03-11 19:01:49 +0100

    Delete ratgame.php

--- ratgame.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-
-$json = file_get_contents('php://input');
-$data = json_decode($json, true);
-
-echo "PHP: name:".$data["name"]." score: ".$data["score"]."\n";
-
-?>
